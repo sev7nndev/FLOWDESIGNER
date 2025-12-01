a922f1c555b0c05888770c2aa493ec295e5b3e25
Seven Beatx <sevenbeatx@gmail.com>
2025-12-01 10:37:31 -0300

    [dyad] Refining animated border button thickness - wrote 1 file(s)

--- components/MovingBorderButton.tsx
+++ components/MovingBorderButton.tsx
@@ -47,7 +47,7 @@ export function MovingBorderButton({
         <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
             className={cn(
-              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(theme(colors.primary)_40%,transparent_60%)]",
+              "h-10 w-10 opacity-[0.8] bg-[radial-gradient(theme(colors.primary)_40%,transparent_60%)]",
               borderClassName
             )}
           />
