26f36cce37c3672a1cb84b03d4ed8e775666b7ea
Seven Beatx <sevenbeatx@gmail.com>
2025-12-17 18:13:04 -0300

    fix: correct backend path in vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 // Vercel Serverless Function Entry Point
-const app = require('../../backend/server.cjs');
+const app = require('../backend/server.cjs');
 
+// Export as serverless function handler
 module.exports = app;
